86b9b09b151b7b7f901042539ebeab0f8d252345
Chris Jantzen <chris.m.jantzen@gmail.com>
2022-09-02 10:41:20 -0700

    Upgraded AD AutoDoc - added dns, dhcp, primary dc, and at a glance.

--- Active Directory.ps1
+++ Active Directory.ps1
@@ -5,6 +5,23 @@ $orgID = "<ITG Org ID>"
 $LastUpdatedUpdater_APIURL = "<LastUpdatedUpdater API URL>"
 $FlexAssetName = "Active Directory"
 $Description = "A network one-page document that shows the current configuration for Active Directory."
+$ImageURLs = @{
+    'AD Level' = @{
+        'default' = "https://www.seatosky.com/wp-content/uploads/2022/09/active-directory.png"
+        '2000' = "https://www.seatosky.com/wp-content/uploads/2022/09/windows_2000_server.png"
+        '2003' = "https://www.seatosky.com/wp-content/uploads/2022/09/windows-server-2003.png"
+        '2008' = "https://www.seatosky.com/wp-content/uploads/2022/09/server-2008.png"
+        '2008 R2' = "https://www.seatosky.com/wp-content/uploads/2022/09/windows-server-2008-r2.png"
+        '2012' = "https://www.seatosky.com/wp-content/uploads/2022/09/windows-server-2012.png"
+        '2012 R2' = "https://www.seatosky.com/wp-content/uploads/2022/09/Windows-2012-R2.png"
+        '2016' = "https://www.seatosky.com/wp-content/uploads/2022/09/windows-server-2016.png"
+    }
+    'DNS Server' = "https://www.seatosky.com/wp-content/uploads/2022/09/dns.png"
+    'DHCP Server' = "https://www.seatosky.com/wp-content/uploads/2022/09/dhcp.png"
+    'Password Complexity Requirements' = "https://www.seatosky.com/wp-content/uploads/2022/09/password.png"
+    'Dell Server' = "https://www.seatosky.com/wp-content/uploads/2022/08/Dell-logo2.png"
+	'Site Details' = "https://www.seatosky.com/wp-content/uploads/2022/08/DetailsIcon.png"
+}
 #####################################################################
 
 # Ensure they are using the latest TLS version
@@ -26,6 +43,91 @@ Else {
 #Settings IT-Glue logon information
 Add-ITGlueBaseURI -base_uri $APIEndpoint
 Add-ITGlueAPIKey $APIKEy
+
+Function IIf($If, $Then, $Else) {
+    If ($If -IsNot "Boolean") {$_ = $If}
+    If ($If) {If ($Then -is "ScriptBlock") {&$Then} Else {$Then}}
+    Else {If ($Else -is "ScriptBlock") {&$Else} Else {$Else}}
+}
+
+function New-BootstrapSinglePanel {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet('active', 'success', 'info', 'warning', 'danger', 'blank')]
+        [string]$PanelShading,
+        
+        [Parameter(Mandatory)]
+        [string]$PanelTitle,
+
+        [Parameter(Mandatory)]
+        [string]$PanelContent,
+
+        [switch]$ContentAsBadge,
+
+        [string]$PanelAdditionalDetail,
+
+        [Parameter(Mandatory)]
+        [int]$PanelSize = 3
+    )
+    
+    if ($PanelShading -ne 'Blank') {
+        $PanelStart = "<div class=`"col-sm-$PanelSize`"><div class=`"panel panel-$PanelShading`">"
+    }
+    else {
+        $PanelStart = "<div class=`"col-sm-$PanelSize`"><div class=`"panel`">"
+    }
+
+    $PanelTitle = "<div class=`"panel-heading`"><h3 class=`"panel-title text-center`">$PanelTitle</h3></div>"
+
+
+    if ($PSBoundParameters.ContainsKey('ContentAsBadge')) {
+        $PanelContent = "<div class=`"panel-body text-center`"><h4><span class=`"label label-$PanelShading`">$PanelContent</span></h4>$PanelAdditionalDetail</div>"
+    }
+    else {
+        $PanelContent = "<div class=`"panel-body text-center`"><h4>$PanelContent</h4>$PanelAdditionalDetail</div>"
+    }
+    $PanelEnd = "</div></div>"
+    $FinalPanelHTML = "{0}{1}{2}{3}" -f $PanelStart, $PanelTitle, $PanelContent, $PanelEnd
+    return $FinalPanelHTML
+    
+}
+    
+function New-AtAGlancecard {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [boolean]$Enabled,
+
+        [Parameter(Mandatory)]
+        [string]$PanelContent,
+
+        [Parameter(Mandatory)]
+        [string]$ImageURL,
+
+        [Parameter(Mandatory = $false)]
+        [string]$PanelAdditionalDetail = "",
+
+		[Parameter(Mandatory = $false)]
+        [boolean]$InfoShading = $false,
+
+        [Parameter(Mandatory = $false)]
+        [boolean]$SquareIcon = $false
+    )
+
+    $Style = ""
+    if ($SquareIcon) {
+        $Style = "style=`"height: 5vw; margin-left: auto; margin-right: auto;`""
+    }
+
+	if ($InfoShading) {
+		New-BootstrapSinglePanel -PanelShading "info" -PanelTitle "<img class=`"img-responsive`" $Style src=`"$ImageURL`">" -PanelContent $PanelContent -PanelAdditionalDetail $PanelAdditionalDetail -ContentAsBadge -PanelSize 4
+    } elseif ($enabled) {
+        New-BootstrapSinglePanel -PanelShading "success" -PanelTitle "<img class=`"img-responsive`" $Style src=`"$ImageURL`">" -PanelContent $PanelContent -PanelAdditionalDetail $PanelAdditionalDetail -ContentAsBadge -PanelSize 4
+    } else {
+        New-BootstrapSinglePanel -PanelShading "danger" -PanelTitle "<img class=`"img-responsive`" $Style src=`"$ImageURL`">" -PanelContent $PanelContent -PanelAdditionalDetail $PanelAdditionalDetail -ContentAsBadge -PanelSize 4
+    }
+}
   
 function Get-WinADForestInformation {
     $Data = @{ }
@@ -142,9 +244,10 @@ function Get-WinADForestInformation {
   
 $TableHeader = "<table class=`"table table-bordered table-hover`" style=`"width:80%`">"
 $Whitespace = "<br/>"
-$TableStyling = "<th>", "<th style=`"background-color:#4CAF50`">"
+$TableStyling = "<th>", "<th class='bg-info'>"
   
 $RawAD = Get-WinADForestInformation
+$ServerDetails = (Get-CimInstance -Class Win32_ComputerSystem);
   
 $ForestRawInfo = new-object PSCustomObject -property $RawAD.ForestInformation | convertto-html -Fragment | Select-Object -Skip 1
 $ForestNice = $TableHeader + ($ForestRawInfo -replace $TableStyling) + $Whitespace
@@ -168,9 +271,10 @@ $ForestFunctionalLevel = $RawAD.RootDSE.forestFunctionality
 $DomainFunctionalLevel = $RawAD.RootDSE.domainFunctionality
 $domaincontrollerMaxLevel = $RawAD.RootDSE.domainControllerFunctionality
   
-$passwordpolicyraw = Get-ADDefaultDomainPasswordPolicy | Select-Object ComplexityEnabled, PasswordHistoryCount, LockoutDuration, LockoutThreshold, MaxPasswordAge, MinPasswordAge | convertto-html -Fragment -As List | Select-Object -skip 1
+$passwordpolicyraw = Get-ADDefaultDomainPasswordPolicy | Select-Object ComplexityEnabled, PasswordHistoryCount, LockoutDuration, LockoutThreshold, MaxPasswordAge, MinPasswordAge 
+$passwordpolicyhtml = $passwordpolicyraw | convertto-html -Fragment -As List | Select-Object -skip 1
 $passwordpolicyheader = "<tr><th><b>Policy</b></th><th><b>Setting</b></th></tr>"
-$passwordpolicyNice = $TableHeader + ($passwordpolicyheader -replace $TableStyling) + ($passwordpolicyraw -replace $TableStyling) + $Whitespace
+$passwordpolicyNice = $TableHeader + ($passwordpolicyheader -replace $TableStyling) + ($passwordpolicyhtml -replace $TableStyling) + $Whitespace
 
 $adminaccounts = @()
 $adminaccounts += Get-ADGroupMember "Domain Admins" | Select-Object SamAccountName, Name
@@ -196,10 +300,142 @@ if ($DomainLevelSplit.Groups[2].Value) {
 	$DomainLevel += $DomainLevelSplit.Groups[2].Value
 }
 
+# Get all configurations for filtering
+$Configurations = Get-ITGlueConfigurations -page_size "1000" -organization_id $OrgID
+$i = 1
+while ($Configurations.links.next) {
+	$i++
+	$Configurations_Next = Get-ITGlueConfigurations -page_size "1000" -page_number $i -organization_id $OrgID
+	$Configurations.data += $Configurations_Next.data
+	$Configurations.links = $Configurations_Next.links
+}
+$Configurations = $Configurations.data
+
 $DomainControllers = $RawAD.GlobalCatalogs | Foreach-Object { $_ -replace ".$($RawAD.ForestName)$", "" }
 $DCAssets = @()
 foreach ($DC in $DomainControllers) {
-    $DCAssets += (Get-ITGlueConfigurations -page_size "1000" -filter_name $DC -organization_id $OrgID).data
+    $DCAssets += $Configurations | Where-Object { $_.attributes.Name -like $DC }
+}
+
+$PrimaryDomainControllers = Get-ADDomainController -Discover -Domain $RawAD.ForestName -Service "PrimaryDC"
+$PrimaryDCAssets = @()
+foreach ($DC in $PrimaryDomainControllers) {
+    if ($DC.Name -in $DCAssets.attributes.name) {
+        $PrimaryDCAssets +=  $DCAssets | Where-Object { $_.attributes.name -eq $DC.Name } | Select-Object -First 1
+    } else {
+        $PrimaryDCAssets += $Configurations | Where-Object { $_.attributes.Name -like $DC.Name }
+    }
+}
+
+$AllDHCPServers = Get-DhcpServerInDC
+$DHCPServers = @()
+foreach ($Server in $AllDHCPServers) {
+    $Lookup = $false
+    try {
+        $Lookup = [System.Net.Dns]::GetHostbyAddress($Server.IPAddress.IPAddressToString)
+    } catch {}
+    if ($Lookup) {
+        $Hostname = ($Lookup.Hostname.replace($RawAD.ForestName, "")).Trim(".")
+        $DHCPAsset = $Configurations | Where-Object { $_.attributes.Name -like $Hostname }
+        if (!$DHCPAsset) {
+            $Hostname = ($Lookup.Hostname -split ".")[0]
+            $DHCPAsset = $Configurations | Where-Object { $_.attributes.Name -like $Hostname -or $_.attributes.'primary-ip' -like $Server.IPAddress.IPAddressToString }
+        }
+        if ($DHCPAsset) {
+            $DHCPServers += $DHCPAsset
+        }
+    }
+}
+
+$AllDNSServers = Resolve-DnsName -Name $RawAD.ForestName
+$DNSServers = @()
+foreach ($Server in $AllDNSServers) {
+    $Lookup = $false
+    try {
+        $Lookup = [System.Net.Dns]::GetHostbyAddress($Server.IPAddress)
+    } catch {}
+    if ($Lookup) {
+        $Hostname = ($Lookup.Hostname.replace($RawAD.ForestName, "")).Trim(".")
+        $DNSAsset = $Configurations | Where-Object { $_.attributes.Name -like $Hostname }
+        if (!$DNSAsset) {
+            $Hostname = ($Lookup.Hostname -split ".")[0]
+            $DNSAsset = $Configurations | Where-Object { $_.attributes.Name -like $Hostname -or $_.attributes.'primary-ip' -like $Server.IPAddress.IPAddressToString }
+        }
+        if ($DNSAsset) {
+            $DNSServers += $DNSAsset
+        }
+    }
+}
+
+
+# At a Glance Summary
+$AtAGlanceHash = [ordered]@{
+    'AD Level'          = $true
+    'Site Details'       = $true
+    'Dell Server'       = if ($ServerDetails.Manufacturer -like '*Dell*') { $true } else { $false }
+    'DNS Server'        = if (($DNSServers | Measure-Object).Count -gt 0) { $true } else { $false }
+    'DHCP Server'       = if (($DHCPServers | Measure-Object).Count -gt 0) { $true } else { $false }
+    'Password Complexity Requirements' = if ($passwordpolicyraw -and $passwordpolicyraw.ComplexityEnabled) { $true } else { $false }
+}
+
+$i = 0
+$ATaGlanceHTML = foreach ($Hash in $AtAGlanceHash.GetEnumerator()) {
+    if ($i -eq 0) {
+        "<div class='row'>"
+    }
+    $AdditionalDetails = ""
+    $DomainLevelImage = $false
+    if ($Hash.name -eq 'AD Level' -and $Hash.value) {
+        $AdditionalDetails = "<strong>$DomainLevel</strong><br>"
+        $AdditionalDetails += "<span style=`"text-decoration: underline;`">$($RawAD.ForestName)</span><br>";
+        if ($PrimaryDCAssets) {
+            foreach ($Asset in $PrimaryDCAssets) {
+                $AdditionalDetails += "<a href=`"$($Asset.attributes.'resource-url')`">$($Asset.attributes.name)</a>, ";
+            }
+            $AdditionalDetails = $AdditionalDetails.Substring(0, $AdditionalDetails.Length-2)
+        }
+        
+        if ($ImageURLs[$hash.name][$DomainLevel]) {
+            $DomainLevelImage = $ImageURLs[$hash.name][$DomainLevel]
+        } else {
+            $DomainLevelImage = $ImageURLs[$hash.name]['default']
+        }
+    } elseif ($Hash.name -eq 'Site Details' -and $Hash.value) {
+        $AdditionalDetails = "Sites count: $($RawAD.ForestInformation.'Sites Count')";
+        $AdditionalDetails += "<br />Enabled Users: $EnabledUsers <br>Domain Admins: $AdminUsers";
+    } elseif ($Hash.name -eq 'Dell Server') {
+        if ($hash.value) {
+            $AdditionalDetails = $ServerDetails.Model
+        } else {
+            $AdditionalDetails = $ServerDetails.Manufacturer + " " + $ServerDetails.Model
+        }
+    }
+    
+    New-AtAGlancecard -Enabled $hash.value -PanelContent $hash.name -ImageURL (IIf $DomainLevelImage $DomainLevelImage $ImageURLs[$hash.name]) -PanelAdditionalDetail $AdditionalDetails -SquareIcon ($Hash.name -notin @("Dell Server", "DHCP Server")) -InfoShading ($Hash.name -eq "Site Details")
+    $i++
+    if ($i % 3 -eq 0) {
+        "</div><div class='row'>"
+    }
+}
+
+$PhysicalConfig = if ($AtAGlanceHash.'Dell server' -eq $true) {
+
+    $Preferences = omconfig preferences cdvformat delimiter=pipe
+    [xml]$ControllerList = (omreport storage controller -fmt xml)
+    $DiskLayoutRaw = foreach ($Controller in $ControllerList.oma.controllers.DCStorageObject.GlobalNo.'#text') {
+        omreport storage pdisk controller=$Controller -fmt cdv
+    }
+
+    "<h4>Disks</h4>"
+    ($DiskLayoutRaw |  select-string -SimpleMatch "ID|Status|" -context 0, ($DiskLayoutRaw).Length | convertfrom-csv -Delimiter "|" | Select-Object Name, Status, Capacity, State, "Bus Protocol", "Product ID", "Serial No.", "Part Number", Media | Where-Object { $_.Name -and $_.Name -ne "Name" } | convertto-html -Fragment)
+    $DiskNumbers = (0..1000)
+    $RAIDLayoutRaw = omreport storage vdisk -fmt cdv
+
+    "<br /><h4>RAID</h4>"
+    ($RAIDLayoutRaw |  select-string -SimpleMatch "ID|Status|" -context 0, ($RAIDLayoutRaw).Length | convertfrom-csv -Delimiter "|" | Select-Object '> ID', Name, Status, State, Layout, "Device Name", "Read Policy", "Write Policy", Media | Where-Object { $_.'> ID' -and $_.'> ID' -in $DiskNumbers -and $_.Name -ne "Name"} |  convertto-html -Fragment)
+
+} else {
+    "Could not retrieve physical host settings - This server is not a Dell Physical machine"
 }
   
 # Get the flexible assets ID
@@ -216,10 +452,14 @@ if (!$ExistingFlexAsset) {
 			'organization-id' = $orgID
 			'flexible-asset-type-id' = $FilterID.id
 			traits = @{
+                'at-a-glance'               = ($ATaGlanceHTML | Out-String)
 				'ad-full-name'              = $RawAD.ForestName
 				'ad-short-name'				= $DomainShortName
                 'ad-level'					= $DomainLevel
-                'ad-servers'                = $DCAssets.ID
+                'ad-servers'                = @($DCAssets.ID)
+                'primary-domain-controller' = @($PrimaryDCAssets.ID)
+                'dns-servers'               = @($DNSServers.ID | Get-Unique)
+                'dhcp-servers'              = @($DHCPServers.ID | Get-Unique)
 				'forest-summary'            = $ForestNice
 				'site-summary'              = $SiteNice
 				'domain-controllers'        = $DCNice
@@ -228,6 +468,7 @@ if (!$ExistingFlexAsset) {
 				'upn-suffixes'              = $UPNNice
 				'default-password-policies' = $passwordpolicyNice
 				'domain-admins'             = $adminsnice
+                'physical-host-configuration' = ($PhysicalConfig | Out-String)
 				'user-count'                = $Users
 			}
 		}
@@ -242,10 +483,14 @@ else {
 		type       = 'flexible-assets'
 		attributes = @{
 			traits = @{
+                'at-a-glance'               = ($ATaGlanceHTML | Out-String)
 				'ad-full-name'              = $RawAD.ForestName
 				'ad-short-name'				= $DomainShortName
                 'ad-level'					= $DomainLevel
-                'ad-servers'                = $DCAssets.ID
+                'ad-servers'                = @($DCAssets.ID)
+                'primary-domain-controller' = @($PrimaryDCAssets.ID)
+                'dns-servers'               = @($DNSServers.ID | Get-Unique)
+                'dhcp-servers'              = @($DHCPServers.ID | Get-Unique)
 				'forest-summary'            = $ForestNice
 				'site-summary'              = $SiteNice
 				'domain-controllers'        = $DCNice
@@ -254,6 +499,7 @@ else {
 				'upn-suffixes'              = $UPNNice
 				'default-password-policies' = $passwordpolicyNice
 				'domain-admins'             = $adminsnice
+                'physical-host-configuration' = ($PhysicalConfig | Out-String)
 				'user-count'                = $Users
 			}
 		}
